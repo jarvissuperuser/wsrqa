f4f1cb1fc69d4905c2eabb55ac891a53f5598778
Timothy Mugadza <mugadzatt001@gmail.com>
2018-03-08 16:21:13 +0200

    data sqlite

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -8,7 +8,7 @@ var async = require("async");
 
 const desktopAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36';
 const modbileAgent = '';
-var db =  new dbl("../app.db");
+var dbi = new dbl("../app.db");
 let project = '';
 let isMobile = '';
 let runTests = '';
@@ -17,168 +17,176 @@ let pivotImg = '';
 let fileName = '';
 let QueueName = '';
 var filesExist;
+var project_id;
 var channelWrapper;
 var timestamp;
-var connection = amqp.connect(['amqp://admin:root1234@localhost:5672']) ;//guest only when client is local on server host
+var connection = amqp.connect(['amqp://admin:root1234@localhost:5672']); //guest only when client is local on server host
 
 var options = {
-	screenSize : {
-		width : 1920,
-		height : 1080
-  },
-	shotSize : {
-		width : "all",
-		height : "all"
-	},
-	userAgent : desktopAgent
+    screenSize: {
+        width: 1920,
+        height: 1080
+    },
+    shotSize: {
+        width: "all",
+        height: "all"
+    },
+    userAgent: desktopAgent
 };
 
 var testurls = {
-	"tl_home" : "www.timeslive.co.za",
-	"tl_article" : "www.timeslive.co.za/politics/2018-01-18-ramaphosa-piles-pressure-on-zuma-with-anti-corruption-call" ,
-	"bl_home" : "www.businesslive.co.za",
-	"sl_home" : "www.sowetanlive.co.za" ,
-	"w_home" : "wantedonline.co.za"
+    "tl_home": "www.timeslive.co.za",
+    "tl_article": "www.timeslive.co.za/politics/2018-01-18-ramaphosa-piles-pressure-on-zuma-with-anti-corruption-call",
+    "bl_home": "www.businesslive.co.za",
+    "sl_home": "www.sowetanlive.co.za",
+    "w_home": "wantedonline.co.za"
 };
-var projects={
-	"tl_home":"timeslive",
-	"tl_article":"timeslive",
-	"bl_live":"businesslive",
-	"sl_home":"sowetanlive",
-	"w_home":"wanted"
+var projects = {
+    "tl_home": "timeslive",
+    "tl_article": "timeslive",
+    "bl_live": "businesslive",
+    "sl_home": "sowetanlive",
+    "w_home": "wanted"
 };
 
 var testLocations;
 
-const runDiff  = (name,timestamp) => {
+const runDiff = (name, timestamp) => {
 
-	try{
-		if(filesExist.pivot&&filesExist.test)
-		resemble(pivotImg)
-		.compareTo(testImg).ignoreNothing().onComplete(function(data){
-			if (data.misMatchPercentage > 5){
-				console.log("name:" + name + ",datafailed:true",'./public/images/' + project + '/' + name + '_' + timestamp + '_diff.png');
-				data.getDiffImage().pack().pipe(fs.
-				createWriteStream('./public/images/' + project + '/' + name + '_' + timestamp + '_diff.png'));
-			}else{
-				console.log("name:"+name+",datafailed:false");
-			}
+    try {
+        if (filesExist.pivot && filesExist.test)
+            resemble(pivotImg)
+            .compareTo(testImg).ignoreNothing().onComplete(function(data) {
+                if (data.misMatchPercentage > 5) {
+                    console.log("name:" + name + ",datafailed:true", './public/images/' + project + '/' + name + '_' + timestamp + '_diff.png');
+                    data.getDiffImage().pack().pipe(fs.createWriteStream('./public/images/' + project + '/' + name + '_' + timestamp + '_diff.png'));
+                } else {
+                    console.log("name:" + name + ",datafailed:false");
+                }
 
-		});
-		else
-			throw ("runnDiff error");
-	}catch(ex){
-		console.log(ex,"no file found");
-	}
+            });
+        else
+            throw ("runnDiff error");
+    } catch (ex) {
+        console.log(ex, "no file found");
+    }
 };
 
-var checkFilesP = (resolve,reject)=>{
-	let parentDir = getParentDir(fileName);
-	fs.readdir(parentDir,(err,files)=>{
-			if (!err){
-				console.log("listing files");
-				files.forEach(file=>{
-					if (file === extractFile(pivotImg)){
-						filesExist["pivot"] = true;
-					}
-					if (file === extractFile(testImg)){
-						filesExist["test"] = true;
-					}
-				});
-				console.log(filesExist,extractFile(filesExist.test?testImg:pivotImg),
-					"list file .done");
-				resolve();
-			}
-			else{
-				fs.emptyDir('./public/images/' + project + '/', err => {
-					if (err){
-						console.log("files error",err);
-						reject(process.exit('0'));
-					}
-					console.log("Creating Project here", err);
-					resolve();
-				});
-			}
-		});
+var checkFilesP = (resolve, reject) => {
+    let parentDir = getParentDir(fileName);
+    fs.readdir(parentDir, (err, files) => {
+        if (!err) {
+            console.log("listing files");
+            files.forEach(file => {
+                if (file === extractFile(pivotImg)) {
+                    filesExist["pivot"] = true;
+                }
+                if (file === extractFile(testImg)) {
+                    filesExist["test"] = true;
+                }
+            });
+            console.log(filesExist, extractFile(filesExist.test ? testImg : pivotImg),
+                "list file .done");
+            resolve();
+        } else {
+            fs.emptyDir('./public/images/' + project + '/', err => {
+                if (err) {
+                    console.log("files error", err);
+                    reject(process.exit('0'));
+                }
+                console.log("Creating Project here", err);
+                resolve();
+            });
+        }
+    });
 }
 
-var getScreensP = (resolve,reject) => {
-	fileName = (filesExist.pivot) ?
-	'./public/images/' + project + '/' + name + '_' + timestamp + '.png' : './public/images/' + project + '/' + name + '.png' ;
+var getScreensP = (resolve, reject) => {
+    fileName = (filesExist.pivot) ?
+        './public/images/' + project + '/' + name + '_' + timestamp + '.png' : './public/images/' + project + '/' + name + '.png';
 
-	try {
-		console.log(fileName, "attempt for image");
-		webshot(testLocations, fileName, options, function(err) {
-			console.log("img error or rundiff");
-			if (err){
-				console.log(err);
-				reject(err);
-			}
-				//else if (filesExist.pivot&&filesExist.test)
-		    	//runDiff(name,timestamp);
-			console.log("Building test cases",QueueName);
-			resolve()
-				//res.write("yes:"+err.message);
-			//}
-		});
-	}catch(ex){
-		console.log("exception getscreen",ex);
-	}
+    try {
+        console.log(fileName, "attempt for image");
+        webshot(testLocations, fileName, options, function(err) {
+            console.log("img error or rundiff");
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            //else if (filesExist.pivot&&filesExist.test)
+            //runDiff(name,timestamp);
+            console.log("Building test cases", QueueName);
+            resolve()
+                //res.write("yes:"+err.message);
+                //}
+        });
+    } catch (ex) {
+        console.log("exception getscreen", ex);
+    }
 }
 
 var arrayToPath = (arr) => {
-	return arr.join('/');
+    return arr.join('/');
 }
-var getParentDir = (path) =>{
-	try{
-		var splitPath = path.split('/');
-		splitPath.pop();
-		return  arrayToPath(splitPath);
-	}catch(ex){
-		console.log(ex);
-	}
+var getParentDir = (path) => {
+    try {
+        var splitPath = path.split('/');
+        splitPath.pop();
+        return arrayToPath(splitPath);
+    } catch (ex) {
+        console.log(ex);
+    }
 
 };
-var extractFile= (filePath) =>{
-	var arr = filePath.split("/"); //unix/unix-like
-	//console.log(arr,filePath,"extractFile");
-	arr.reverse();
-	return arr[0];
+var extractFile = (filePath) => {
+    var arr = filePath.split("/"); //unix/unix-like
+    //console.log(arr,filePath,"extractFile");
+    arr.reverse();
+    return arr[0];
 };
 
-module.exports = async(p,m,t) => {
+module.exports = async(p, m, t) => {
 
-	project = p;
-	isMobile = m;
-	runTests = t;
+    project = p;
+    isMobile = m;
+    runTests = t;
 
-	timestamp = moment().format("MM-D-YY-h-mm-s");
-	console.log("Loading Tests app at " + timestamp);
-	name= (projects[p]===undefined)?project:projects[p];
-	testImg = './public/images/' + project + '/' + name + '_' + timestamp + '.png';
-	pivotImg = './public/images/' + project + '/' + name + '.png' ;
-	fileName = (runTests === 'yes') ? testImg: pivotImg ;
-	filesExist = {test:false,pivot:false};
-	QueueName =  name + timestamp;
-	db.multiquery(["insert into test(t_name) values('"+QueueName+"')"]);
-	db.e.on('done',()=>{
-		console.log(db.datamulti);
-	});
-	//preconfig = paths
-	//checkFiles();
-	testLocations = testurls[project];
-	var pr = new Promise(checkFilesP);
-	pr.then(()=>{
-		return new Promise(getScreensP);
-	}).then((gsp)=>{
-		return new Promise(checkFilesP);
-	}).then((f)=>{
-		if (filesExist.test)
-			runDiff(name,timestamp);
-		else
-			console.log("nothing to test':'diff avoided");
-	}).catch((ex)=>{
-		console.error(ex,"app error");
-	});
+    timestamp = moment().format("MM-D-YY-h-mm-s");
+    console.log("Loading Tests app at " + timestamp);
+    name = (projects[p] === undefined) ? project : projects[p];
+    testImg = './public/images/' + project + '/' + name + '_' + timestamp + '.png';
+    pivotImg = './public/images/' + project + '/' + name + '.png';
+    fileName = (runTests === 'yes') ? testImg : pivotImg;
+    filesExist = { test: false, pivot: false };
+    QueueName = name + "_" + timestamp;
+    var stopper = true;
+    dbi.multiquery(["insert into test(t_name) values('" + QueueName + "')"]);
+    dbi.e.on('done', () => {
+        //console.log(db.datamulti[0, 0].length);
+        var d = dbi.datamulti[0];
+        console.log(d);
+        if (d.length !== 0) {
+            dbi.multiquery(["select last_insert_rowid() as rid "]);
+            stopper = false;
+        } else if (d) {
+            console.log(d, stopper,dbi.datamulti);
+            stopper = false;
+        }
+    });
+    //preconfig = paths
+    //checkFiles();
+    testLocations = testurls[project];
+    var pr = new Promise(checkFilesP);
+    pr.then(() => {
+        return new Promise(getScreensP);
+    }).then((gsp) => {
+        return new Promise(checkFilesP);
+    }).then((f) => {
+        if (filesExist.test)
+            runDiff(name, timestamp);
+        else
+            console.log("nothing to test':'diff avoided");
+    }).catch((ex) => {
+        console.error(ex, "app error");
+    });
 };
-
