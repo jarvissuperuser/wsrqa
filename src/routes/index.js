349d39878bcb4462c3cc03cd939ac7db90697538
Timothy Mugadza <mugadzatt001@gmail.com>
2018-03-13 11:35:25 +0200

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,20 @@
+//import { O_WRONLY } from 'constants';
+
 var express = require('express');
 var router = express.Router();
 var dbl = require("../sqlite_con_man");
-
 /* Render The Data for pictures in table for*/
-//var dbo = new dbl("../app.db");
+var dbo = new dbl("../app.db");
+var arr = [];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express', asts: ["what", "is", "new"] });
+    dbo.db.all("select * from test", (err, rows) => {
+        rows.forEach(row => {
+            arr.push(row.t_name);
+        });
+        res.render('index', { title: 'Express', asts: arr });
+    });
+
 });
 
 module.exports = router;
