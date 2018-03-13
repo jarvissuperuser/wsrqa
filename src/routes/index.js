d5dcabeeaf96848c68e06b4a06cbbfd6a2553fad
Timothy Mugadza <mugadzatt001@gmail.com>
2018-03-13 12:58:11 +0200

    security update

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,16 +5,16 @@ var router = express.Router();
 var dbl = require("../sqlite_con_man");
 /* Render The Data for pictures in table for*/
 var dbo = new dbl("../app.db");
-var arr = [];
+var arr = {};
 /* GET home page. */
 router.get('/', function(req, res, next) {
     dbo.db.all("select * from test", (err, rows) => {
         rows.forEach(row => {
-            arr.push(row.t_name);
+            var name = row.t_name;
+            arr[name] =row.id;
         });
         res.render('index', { title: 'Express', asts: arr });
     });
-
 });
 
 module.exports = router;
