d6abd81904065b0168db08e28806535e618bbe40
Timothy Mugadza <mugadzatt001@gmail.com>
2018-03-15 15:07:06 +0200

    display tests

--- src/routes/record.js
+++ src/routes/record.js
@@ -7,19 +7,27 @@ var dbl = require("../sqlite_con_man");
  var dbo = new dbl("../app.db");
 var arr = {};
 var rsa = [];
-/* GET home page. */
+/** helper functions */
+var get_project = (image)=>{
+	var sp = image.split('_');
+	if (sp.length>1)
+		return sp[0]+"_"+sp[1];
+	else 
+		return undefined;
+};
+/* GET record page. */
 router.get('/', function(req, rest, next) {
 		dbo.db.all("select * from log_info where t_id ="+ req.query.test, (err, rows) => {
         rows.forEach(row => {
 						arr = {};
             arr['image'] =row.log_image;
 						arr['info']=row.log_info;
+						arr['project']= get_project(row.log_image);
 						rsa.push(arr);
         });
         rest.render('record', { title: 'Regression for '+req.query.test , asts: rsa });
 				rsa = [];
     });
-
 		//rest.render('record',
 		//{title:'Test Results', 
 		//res:['data','in','this','form']});
