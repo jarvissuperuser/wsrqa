81b8dbbe0fa9378eedab0325d2c1a2ab0c1f2c05
Timothy T Mugadza <mugadzatt001@gmail.com>
2018-04-17 06:31:41 +0200

    uj comp

--- src/middleware/ujcomponent.js
+++ src/middleware/ujcomponent.js
@@ -3,9 +3,9 @@ var moment = require("moment");
 var resemble = require("node-resemble-js");
 var fs = require('fs-extra');
 var dbl = require('../sqlite_con_man');
-var amqp = require('amqp-connection-manager');
 var async = require("async");
 var UJC = require('../userjourney');
+var puppet = require("puppeteer");
 
 var uj = new UJC();
 const desktopAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36';
@@ -54,54 +54,60 @@ var testLocations;
 module.exports = async(p, m, t) => {
 
 	project = p;
-	isMobile = m;
+	testLocations = testurls[p] + appenditure[m];
 	runTests = t;
-	uj.timestamp = moment().format("MM-D-YY-h-mm-s");
-	uj.log = true;
-	uj.project = '';
-	uj.filesExist = {pivot:false,test:false};
-	uj.testImg = '';
-	uj.pivotImg = '';
-	var b_path = "./public/images/";
-	uj.setup(b_path,projects,project);
-	console.log("Loading Tests app at " , uj.timestamp);
-	uj.name = (projects[p] === undefined) ? project : projects[p];
-	try{
-		uj.dbi = new dbl("../app.db");
-		uj.dbSetup();
-	}
-	catch(ex){
-		uj.dbi.multiquery(["insert into test(t_name) values('" + name + "')"]);
-		uj.dbi.e.on('done', () => {
-			//console.log(db.datamulti[0, 0].length);
-			var d = uj.dbi.datamulti[0];
-			stopper = false;
-			uj.dbi.db.all("select id from test order by id desc limit 1", (err, rows) => {
-				rows.forEach((row) => {
-					uj.project_id = row.id;
+	const browser =  await puppet.launch();
+	const page = await browser.newPage();
+
+	await page.goto(testLocations);
+	if (runTests === "yes") {
+		uj.timestamp = moment().format("MM-D-YY-h-mm-s");
+		uj.log = true;
+		uj.project = '';
+		uj.filesExist = {pivot: false, test: false};
+		uj.testImg = '';
+		uj.pivotImg = '';
+		var b_path = "./public/images/";
+		uj.setup(b_path, projects, project);
+		console.log("Loading Tests app at ", uj.timestamp);
+		uj.name = (projects[p] === undefined) ? project : projects[p];
+		try {
+			uj.dbi = new dbl("../app.db");
+			uj.dbSetup();
+		}
+		catch (ex) {
+			uj.dbi.multiquery(["insert into test(t_name) values('" + name + "')"]);
+			uj.dbi.e.on('done', () => {
+				//console.log(db.datamulti[0, 0].length);
+				var d = uj.dbi.datamulti[0];
+				stopper = false;
+				uj.dbi.db.all("select id from test order by id desc limit 1", (err, rows) => {
+					rows.forEach((row) => {
+						uj.project_id = row.id;
+					});
 				});
 			});
+		}
+
+		var pr = new Promise(function (w, f) {
+			uj.testLocations = testurls[project];
+			uj.setup(b_path, projects, p);
+			uj.checkFilesP(w, f);
+		});
+		pr.then(() => {
+			return new Promise(function (w, f) {
+				uj.filesInit(b_path);
+				uj.getScreensP(w, f);
+			});
+		}).then((gsp) => {
+			return new Promise(uj.checkFilesP);
+		}).then((f) => {
+			if (uj.filesExist.test)
+				uj.runDiff(uj.name, uj.timestamp);
+			else
+				console.log("nothing to test':'diff avoided");
+		}).catch((ex) => {
+			console.error(ex, "app error");
 		});
 	}
-
-	var pr = new Promise(function(w,f){
-		uj.testLocations = testurls[project];
-		uj.setup(b_path,projects,p);
-		uj.checkFilesP(w,f);
-	});
-	pr.then(() => {
-		return new Promise(function(w,f){
-			uj.filesInit(b_path);
-			uj.getScreensP(w,f);
-		});
-	}).then((gsp) => {
-		return new Promise(uj.checkFilesP);
-	}).then((f) => {
-		if (uj.filesExist.test)
-			uj.runDiff(uj.name, uj.timestamp);
-		else
-			console.log("nothing to test':'diff avoided");
-	}).catch((ex) => {
-		console.error(ex, "app error");
-	});
 };
