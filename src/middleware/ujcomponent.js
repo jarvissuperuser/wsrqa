b366059d4578ec922c2fae34511c742cb0979d02
Timothy T Mugadza <mugadzatt001@gmail.com>
2018-04-17 05:59:34 +0200

    userjourney comp

--- src/middleware/ujcomponent.js
+++ src/middleware/ujcomponent.js
@@ -1,224 +1,107 @@
-var fs = require("fs-extra");
 var webshot = require("webshot");
+var moment = require("moment");
 var resemble = require("node-resemble-js");
-var dbl = require('./sqlite_con_man');
+var fs = require('fs-extra');
+var dbl = require('../sqlite_con_man');
+var amqp = require('amqp-connection-manager');
+var async = require("async");
+var UJC = require('../userjourney');
 
+var uj = new UJC();
 const desktopAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36';
+const modbileAgent = '';
+var dbi = new dbl("../app.db");
+let project = '';
+let isMobile = '';
+let runTests = '';
+let QueueName = '';
+var connection = amqp.connect(['amqp://admin:root1234@localhost:5672']); //guest only when client is local on server host
 
-var opts = {
-    screenSize: {
-        width: 1920,
-        height: 1080
-    },
-    shotSize: {
-        width: "all",
-        height: "all"
-    },
-    userAgent: desktopAgent
-}; 
-var selfer ;
-class UserJourney{
-    
-    constructor(options,db){
-        this.options = options?options:opts;
-        this.project = '';
-        this.isMobile = '';
-        this.runTests = '';
-        this.testImg = '';
-        this.pivotImg = '';
-        this.diff_img = '';
-        this.fileName = '';
-        this.QueueName = '';
-        this.filesExist = { test: false, pivot: false };
-        this.project_id = 0;
-        this.timestamp = "";
-        this.testLocations = "http://timeslive.co.za";
-        this.name = '';
-        selfer = this;
-        this.log = false;
-        this.dbi = null;
-    }
-    setup(base_path,projects,p){
-        var self = this;
-        var imgBsPath = base_path?base_path:'./public/images/';
-        console.log("Loading Tests app at " + self.timestamp);
-        self.project = p;
-        self.name = (projects === undefined) ? self.project : projects[p];
-        this.filesInit(imgBsPath);
-    }
-    dbSetup(){
-        var self = selfer;
-        this.dbi = new dbl("../app.db");
-        selfer.dbi.multiquery(["insert into test(t_name) values('" + self.name + "')"]);
-        selfer.dbi.e.on('done', () => {
-            var d = self.dbi.datamulti[0];
-            self.dbi.db.all("select id from test order by id desc limit 1", (err, rows) => {
-                rows.forEach((row) => {
-                    self.project_id = row.id;
-                });
-            });
-        });
-    }
-    filesInit(imgBsPath){
-        var self = selfer;
-        self.testImg = self.testImg?self.testImg:imgBsPath + self.project + '/' + self.name + '_' + self.timestamp + '.png';
-        self.pivotImg = self.pivotImg?self.pivotImg:imgBsPath + self.project + '/' + self.name + '.png';
-        self.fileName = (self.filesExist.pivot) ? self.testImg : self.pivotImg;
-        self.QueueName = self.name + "_" + self.timestamp;
-        self.fileName = (self.filesExist.pivot) ?this.testImg 
-            :this.pivotImg;
-        self.diff_img = imgBsPath + self.project + '/' + self.name + '_' + self.timestamp + '_diff.png';
-    }
-    genMessage(opt,mismatch){
-        var msg; 
-        var self = selfer;
-        var insert = "insert into log_info(t_id,log_info,log_image) values(";
-        var update = "";
-        var pID = (self.project_id===undefined?0:self.project_id); 
-        var q= "";
-        var fileFound = self.filesExist.test ? self.testImg : self.pivotImg;
-        switch(opt){
-            case "readdir":
-                msg = "Test Found "+ (self.filesExist.test?"Test Img":"Pivot Img");
-                q =insert+ pID+",\""+ msg+"\",\""
-                    +self.extractFile(fileFound)+"\")";
-                break ;
-            case "emptydir":
-                msg = "Project created here"
-                q =insert+ pID+",\""+ msg+"\",\""
-                    +self.extractFile(fileFound)+"\")";
-                break ;
-            case "mismatchY":
-                msg = "Image Difference :" + mismatch +"\%.";
-                q=insert+ pID+",\""+ msg+"\",\""
-                    +self.extractFile(self.diff_img)+"\")";
-                break ;
-            case "mismatchN":
-                msg = "Image Difference :" + mismatch +"\%.";
-                q=insert+ pID+",\""+ msg+"\",\""
-                    +self.extractFile(fileFound)+"\")";
-                break ;
+var options = {
+	screenSize: {
+		width: 1920,
+		height: 1080
+	},
+	shotSize: {
+		width: "all",
+		height: "all"
+	},
+	userAgent: desktopAgent
+};
 
-            case "update":
-                msg = "";
-                q=update+ pID+",\""+ msg+"\",\""
-                    +self.extractFile(fileFound)+"\")";
-                break ;
-            default:
-                break ;
-        }
-        if (self.log)
-            this.logToDataBase(q);
-    }
-    checkFilesP(resolve, reject) {
-        var self = selfer;
-        let parentDir = self.getParentDir(self.pivotImg);
-        fs.readdir(parentDir, (err, files) => {
-            if (!err) {
-                console.log("listing files >>" ,self.pivotImg, self.testImg);
-                var s = self;
-                files.forEach(file => {
-                    if (file === self.extractFile(self.pivotImg))
-                        self.filesExist["pivot"] = true;
-                    if (file === self.extractFile(self.testImg)) 
-                        self.filesExist["test"] = true;
-                    if (!self.log)
-                        console.log(file);
-                });
-                var fileFound = self.filesExist.test ? self.testImg : self.pivotImg;
-                console.log(self.filesExist, self.extractFile(fileFound),
-                    "list file .done");
-                self.genMessage("readdir");
-                resolve();
-            } else {
-                fs.emptyDir(parentDir, err1 => {
-                    if (err1) {
-                        console.log("files error", err);
-                        //reject(process.exit('0'));
-                    }
-                    self.genMessage("emptydir");
-                    console.log("Creating Project here", err);
-                    resolve();
-                });
-            }
-        });
-    }
-    getScreensP (resolve, reject) {
-        var self = selfer;
-
-        try {
-            console.log(self.fileName, "attempt for image");
-            webshot(self.testLocations, self.fileName, self.options, function(err) {
-                console.log("img error or rundiff");
-                if (err) {
-                    console.log(err.trace);
-                    reject(err);
-                }
-                console.log("Building test cases", self.fileName);
-                resolve();
-            });
-        } catch (ex) {
-            console.log("exception getscreen", ex);
-        }
-    }
-    arrayToPath (arr) {
-        return arr.join('/');
-    }
-    getParentDir (path)  {
-        try {
-            var splitPath = path.split('/');
-            splitPath.pop();
-            return this.arrayToPath(splitPath);
-        } catch (ex) {
-            console.log(ex);
-        }
-    }
-    extractFile (filePath) {
-        var arr = filePath.split("/"); //unix/unix-like
-        arr.reverse();
-        return arr[0];
-    }
-    runDiff (name, timestamp) {
-        var self = this;
-        try {
-            if (self.filesExist.pivot && self.filesExist.test)
-                resemble(self.pivotImg)
-                .compareTo(self.testImg).ignoreNothing().onComplete(function(data) {
-                    if (data.misMatchPercentage > 5) {
-                        console.log("name:" + name + ",datafailed:true", self.diff_img);
-                        data.getDiffImage().pack().pipe(fs.createWriteStream(self.diff_img));
-                        self.genMessage("mismatchY",data.misMatchPercentage);
-                    } else {
-                        console.log("name:" + name + ",datafailed:false");
-                        self.genMessage("mismatchN",data.misMatchPercentage);
-                    }
-                    self.logToDataBase("update test set t_val='"                    
-                        +data.misMatchPercentage + "' where id="+self.project_id+";");
-                });
-            else
-                throw ("runDiff error ");
-        } catch (ex) {
-            console.log(ex, "testfiles not found");
-        }
-    }
-
-    logToDataBase (qry) {
-        var self = selfer;
-        try{
-            if (this.project_id !== 0)
-                selfer.dbi.db.all(qry, function(err, row) {
-                    if (err){
-                        console.log(err,qry,"logToDb Failed");
-                    }
-                    else
-                        console.log(qry,"log in to db");
-                });
-            else{
-                            console.log('no project_id');
-            }
-        }catch(ex){
-            console.log("logToDataBase failed",ex,qry);
-        }
-    }
+var appenditure = {
+	"login":"/u/sign-in/",
+	"register":"u/sign-up/",
+	"reset":"/u/reset/"
 }
-module.exports = UserJourney;
+
+var testurls = {
+	"tl_home": "www.timeslive.co.za",
+	"bl_home": "www.businesslive.co.za",
+	"sl_home": "www.sowetanlive.co.za",
+	"w_home": "wantedonline.co.za"
+};
+var projects = {
+	"tl_home": "timeslive",
+	"tl_article": "timeslive",
+	"bl_home": "businesslive",
+	"sl_home": "sowetanlive",
+	"w_home": "wanted"
+};
+
+var testLocations;
+
+module.exports = async(p, m, t) => {
+
+	project = p;
+	isMobile = m;
+	runTests = t;
+	uj.timestamp = moment().format("MM-D-YY-h-mm-s");
+	uj.log = true;
+	uj.project = '';
+	uj.filesExist = {pivot:false,test:false};
+	uj.testImg = '';
+	uj.pivotImg = '';
+	var b_path = "./public/images/";
+	uj.setup(b_path,projects,project);
+	console.log("Loading Tests app at " , uj.timestamp);
+	uj.name = (projects[p] === undefined) ? project : projects[p];
+	try{
+		uj.dbi = new dbl("../app.db");
+		uj.dbSetup();
+	}
+	catch(ex){
+		uj.dbi.multiquery(["insert into test(t_name) values('" + name + "')"]);
+		uj.dbi.e.on('done', () => {
+			//console.log(db.datamulti[0, 0].length);
+			var d = uj.dbi.datamulti[0];
+			stopper = false;
+			uj.dbi.db.all("select id from test order by id desc limit 1", (err, rows) => {
+				rows.forEach((row) => {
+					uj.project_id = row.id;
+				});
+			});
+		});
+	}
+
+	var pr = new Promise(function(w,f){
+		uj.testLocations = testurls[project];
+		uj.setup(b_path,projects,p);
+		uj.checkFilesP(w,f);
+	});
+	pr.then(() => {
+		return new Promise(function(w,f){
+			uj.filesInit(b_path);
+			uj.getScreensP(w,f);
+		});
+	}).then((gsp) => {
+		return new Promise(uj.checkFilesP);
+	}).then((f) => {
+		if (uj.filesExist.test)
+			uj.runDiff(uj.name, uj.timestamp);
+		else
+			console.log("nothing to test':'diff avoided");
+	}).catch((ex) => {
+		console.error(ex, "app error");
+	});
+};
