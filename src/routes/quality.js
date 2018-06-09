01477b34f11303eb0f2c603d63c08c57596a4b24
Timothy T Mugadza <mugadzatt001@gmail.com>
2018-06-09 13:51:19 +0200

    post ready

--- src/routes/quality.js
+++ src/routes/quality.js
@@ -6,6 +6,10 @@ router.get('/new', async(req, rest, next) =>{
     rest.render('quality',{title:'New QA Reporting',view:'new'});
 }).get('/qa', async(req, rest, next) =>{
     rest.render('quality',{title:'QA by WhoX',view:'adding'});
+}).post('/add',async(req, rest, next) =>{
+    //rest.render('quality',{title:'QA by WhoX',view:'adding'});
+    let q=req.body;
+    rest.send(JSON.stringify([q]));
 });
 
 module.exports = router ;
