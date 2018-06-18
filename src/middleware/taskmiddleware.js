70d372ef636e7215d6e3da178b2ee39ae9196873
timothy <mugadzatt001@gmail.com>
2018-06-18 10:07:04 +0200

    testcase class

--- src/middleware/taskmiddleware.js
+++ src/middleware/taskmiddleware.js
@@ -1,8 +1,10 @@
-let QB = require("../querybuilder");
+const QB = require("../querybuilder");
 const dbi = require("../sqlite_con_man");
+const TCASE = require("../qualityCaseMan");
 let db = new dbi("../app.db");
-let result = [];
 
+let tcase = new TCASE();
+let result = [];
 let qb = new QB;
 let new_obj = {};
 let qry = "";
@@ -13,93 +15,22 @@ module.exports = async(req,rest)=>{
 
     switch (req.submit){
         case "get_case_reports":
-            
+            tcase.get_tests(req,rest);
             break;
         case "add_case_reg":
-            new_obj = qb.mute(req,{},['submit']);
-            new_obj = qb.correc(new_obj,{},'tcr_');
-            qry = qb.insert("test_case_reg",qb.ex_key(new_obj,[]),qb.ex_val(new_obj,[]));  
-            db.db.all(qry, (err,rows) => {
-                if (!err)
-                    return new Promise((resolve)=> {
-                        rest.write(JSON.stringify({figure:''}));
-                        rest.end();
-                        resolve();
-                    }).catch((err)=>{
-                        rest.write(JSON.stringify(err));
-                        rest.end();
-                    });
-                else {
-                    rest.write(JSON.stringify([err,"limit",result]));
-                    rest.end();
-                }
-            });
+            tcase.add_test_reg(req,rest);
             break;
         case "update_case_reg":
+            tcase.update_test_reg(req,rest);
             break;
         case "add_case":
-            new_obj = qb.mute(req,{},['submit','id']);
-            new_obj = qb.correc(new_obj,{},'tc_');
-            new_obj['tcr_id'] = req.id; 
-            qry = qb.insert("test_cases",qb.ex_key(new_obj,[]),qb.ex_val(new_obj,[]));  
-            db.db.all(qry, (err,rows) => {
-                if (!err)
-                    return new Promise((resolve)=> {
-                        rest.write(JSON.stringify({msg:"added"}));
-                        rest.end();
-                        resolve();
-                    }).catch((err)=>{
-                        rest.write(JSON.stringify(err));
-                        rest.end();
-                    });
-                else {
-                    rest.write(JSON.stringify([err.message,"limit",qry]));
-                    rest.end();
-                }
-            });
+            tcase.add_test(req,rest);
             break;
         case "update_case":
-        new_obj = qb.mute(req,{},['submit']);
-        new_obj = qb.correc(new_obj,{},'tcr_');
-        qry = qb.insert("test_cases",qb.ex_key(new_obj),qb.ex_val(new_obj));  
-        db.db.all(qry, (err,rows) => {
-            if (!err)
-                return new Promise((resolve)=> {
-                    rest.write(JSON.stringify({msg:updated}));
-                    rest.end();
-                    resolve();
-                }).catch((err)=>{
-                    rest.write(JSON.stringify(err));
-                    rest.end();
-                });
-            else {
-                rest.write(JSON.stringify([err,"limit",result]));
-                rest.end();
-            }
-        });
-        break;
+            tcase.update_test_reg(req,rest);
             break;
-        case "get_cases": 
-            let tc_id = req.id ;
-            db.db.all(qb.slct("*","test_cases","tcr_id="+tc_id+" "+limit),
-            (err,rows)=>{
-                if (!err)
-                    return new Promise((resolve)=> {
-                        rows.forEach(r => {
-                            result.push(r);
-                        });
-                        rest.write(JSON.stringify(result));
-                        rest.end();
-                        resolve();
-                    }).catch((err)=>{
-                        rest.write(JSON.stringify(err));
-                        rest.end();
-                    });
-                else {
-                    rest.write(JSON.stringify([err,"limit",result]));
-                    rest.end();
-                }
-            });
+        case "get_cases":
+            tcase.get_test(req,rest);
             break;
         default:
             rest.write(JSON.stringify([]));
--- src/qualityCaseMan.js
+++ src/qualityCaseMan.js
@@ -4,47 +4,80 @@ let qb = new QB();
 let db = new sqlcon("../app.db");
 
 
-class TCase{
-    constructor(){}
-    add_test_reg(req,rest){
-        return new Promise((r)=>{
+class TCase {
+    constructor() { }
+    add_test_reg(req, rest) {
+        let render_rows = this.render_rows;
+        return new Promise((r) => {
+            new_obj = qb.mute(req, {}, ['submit']);
+            new_obj = qb.correc(new_obj, {}, 'tcr_');
+            qry = qb.insert("test_case_reg", qb.ex_key(new_obj, []), qb.ex_val(new_obj, []));
+            db.db.all(qry, (err, rows) => {
+                render_rows(req, rest, { msg: 'added' }, err);
+                r();
+            });
+        }).catch((err) => {
+            rest.write(JSON.stringify(err));
+            rest.end();
+        });;
+    }
+    update_test_reg(req, rest) {
+        let render_rows = this.render_rows;
+        return new Promise((r) => {
+            new_obj = qb.mute(req, {}, ['submit']);
+            new_obj = qb.correc(new_obj, {}, 'tcr_');
+            qry = qb.insert("test_cases", qb.ex_key(new_obj), qb.ex_val(new_obj));
+            db.db.all(qry, (err, rows) => {
+                render_rows(req, rest, { msg: 'updated' }, err);
+                r();
+            });
         });
     }
-    update_test_reg(req,rest){
-        return new Promise((r)=>{
-            
-        });
-    }
-    get_test_reg(req,rest){
-        return new Promise((r)=>{
-            db.db.all(qb.slct("*","test_case_reg","1=1 "+limit),
-            (err,rows)=>{
-                if (!err){
+    get_tests() {
+        let render_rows = this.render_rows;
+        let result = [];
+        return new Promise((r) => {
+            db.db.all(qb.slct("*", "test_case_reg", "1=1 " + limit),
+                (err, rows) => {
+                    if (rows)
                         rows.forEach(r => {
                             result.push(r);
                         });
-                        rest.write(JSON.stringify(result));
-                        rest.end();
-                        r();
-                    }
-                else {
-                    rest.write(JSON.stringify([err,"limit",result]));
-                    rest.end();
+                    render_rows(req, rest, rows, err);
                     r();
-                }
+                });
+        });
+    }
+    add_test(req, rest) {
+        let render_rows = this.render_rows;
+        return new Promise((r) => {
+            new_obj = qb.mute(req, {}, ['submit', 'id']);
+            new_obj = qb.correc(new_obj, {}, 'tc_');
+            new_obj['tcr_id'] = req.id;
+            qry = qb.insert("test_cases", qb.ex_key(new_obj, []), qb.ex_val(new_obj, []));
+            db.db.all(qry, (err) => {
+                render_rows(req, rest, { msg: "added" }, err);
+                r();
             });
-
+        }).catch((err) => {
+            rest.write(JSON.stringify(err));
+            rest.end();
+        });;
+    }
+    get_test(req, rest) {
+        return new Promise((r) => {
+            r();
         });
     }
-    add_test(req,rest){
-        return new Promise((r)=>{
-            
-        });
-    }
-    get_test(req,rest){
-        return new Promise((r)=>{
-            
-        });
+    render_rows(req, rest, obj, err) {
+        if (!err) {
+            rest.write(JSON.stringify(obj));
+        }
+        else {
+            rest.write(JSON.stringify([err.message, "limit", qry]));
+        }
+        rest.end();
     }
 
 }
+module.exports = TCase;
