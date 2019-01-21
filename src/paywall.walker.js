0dd27074c446494de9904bb45d5e7ccf248ff3e3
timothy <mugadzatt001@gmail.com>
2019-01-21 09:13:13 +0200

    Paywall Walker clean

--- src/paywall.walker.js
+++ src/paywall.walker.js
@@ -4,6 +4,9 @@ const CFG = require("./setup");
 let u = new UJMan();
 let config = new CFG();
 const p = 'tl';
+let success = false;
+let is_logged_in = false;
+let build = null;
 config.init("./app.ini");
 (async function main() {
 	await u.initBrowser('1366x768');
@@ -13,9 +16,20 @@ config.init("./app.ini");
 	await u.gsFailOver();
 	console.log("btns :",btns.length);
 	for(let i = 0; i < btns.length;i++){
-		await u.page.$eval(".subscribe",btn => btn[i].click());
-		await u.gsFailOver();
-		await u.page.goto(config.get_url(p,'buy'));
+		await u.page.waitFor(3000);
+		await u.page.$eval(".subscribe",btn => {
+			try {
+				console.log(btn);
+				btn.click();
+			}catch (e) {
+				console.log(btn)
+			}
+		}).catch((e)=>console.log(e));
+		if (false) {
+			await u.gsFailOver();
+			await u.page.goto(config.get_url(p, 'buy'));
+		}
 	}
+	u.page.waitFor(4000);
 	await u.browser.close();
 })();
